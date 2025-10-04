1b2e07785552f3c2852019fc662d5fcfc167db6e
developer-nur <nursweb6@gmail.com>
2025-10-04 19:58:37 +0600

    string method applied

---
+++ stringMethod.js
@@ -0,0 +1,13 @@
+console.log("Hi there");
+
+// slice()
+const text = "Bangladesh";
+
+console.log(text.slice(0, 6));
+
+
+// subsrt()
+
+const subStr = "Apple, Banana, Orange"
+const newStr = subStr.substr(7, 6)
+console.log(newStr);

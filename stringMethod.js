ef11c9dcdd5f9fa8e6aa1074db6804fc3a970623
developer-nur <nursweb6@gmail.com>
2025-10-05 20:33:58 +0600

    array js added

--- stringMethod.js
+++ stringMethod.js
@@ -1,13 +1,22 @@
 console.log("Hi there");
 
 // slice()
-const text = "Bangladesh";
-
-console.log(text.slice(0, 6));
+const country = "Bangladesh";
+const language = country.slice(0, 6)
+console.log(language); //// output Bangla
 
 
 // subsrt()
-
 const subStr = "Apple, Banana, Orange"
 const newStr = subStr.substr(7, 6)
-console.log(newStr);
+console.log(newStr); // output Banana
+
+// replace()
+const rep = "I love anny";
+const replaced = rep.replace("anny", "Anny")
+console.log(replaced); // output I love Anny
+
+// trim()
+const remove = "    Hi!   ";
+const removed = remove.trim()
+console.log(removed); // output Hi! 
